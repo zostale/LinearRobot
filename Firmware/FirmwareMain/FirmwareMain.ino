a5f0f8158347e9c24afdad37b0736b04bd69d34a
Shyamal Anadkat <shyamalanadkat@gmail.com>
2016-03-28 12:21:32 -0500

    Update FirmwareMain.ino

--- Firmware/FirmwareMain/FirmwareMain.ino
+++ Firmware/FirmwareMain/FirmwareMain.ino
@@ -4,6 +4,9 @@
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_PWMServoDriver.h"
+/*
+TODO : missing header files
+*/
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 Adafruit_StepperMotor *myMotor = AFMS.getStepper(200, 2);
